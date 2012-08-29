affcae97fd7f418d9abd71f0384363717bcd2434
Theo Andersen <andersen.theo@gmail.com>
2012-08-29 22:43:09 +0200

    Added key-bindings.el which was accidently deleted in last commit, and caused a hell of problems

---
+++ key-bindings.el
@@ -0,0 +1,222 @@
+;; I don't need to kill emacs that easily
+;; the mnemonic is C-x REALLY QUIT
+(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-x C-c") 'delete-frame)
+
+;; Completion that uses many different methods to find options.
+(global-set-key (kbd "C-.") 'hippie-expand)
+(global-set-key (kbd "C-:") 'hippie-expand-lines)
+
+;; Smart M-x
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; Expand region (increases selected region by semantic units)
+(global-set-key (if is-mac (kbd "C-@") (kbd "C-'")) 'er/expand-region)
+
+;; Experimental multiple-cursors
+(global-set-key (kbd "C-S-c C-S-c") 'mc/add-multiple-cursors-to-region-lines)
+(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
+
+;; Mark additional regions matching current region
+(global-set-key (kbd "M-æ") 'mark-all-like-this)
+(global-set-key (kbd "C-å") 'mark-previous-like-this)
+(global-set-key (kbd "C-æ") 'mark-next-like-this)
+(global-set-key (kbd "C-Å") 'mark-more-like-this-extended)
+(global-set-key (kbd "C-Æ") 'mark-more-like-this-extended)
+
+;; Replace rectangle-text with inline-string-rectangle
+(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
+
+;; Quickly jump in document with ace-jump-mode
+(define-key global-map (kbd "C-ø") 'ace-jump-mode)
+
+;; Perform general cleanup.
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+(global-set-key (kbd "C-c C-<return>") 'delete-blank-lines)
+
+;; Turn on the menu bar for exploring new modes
+(global-set-key (kbd "C-<f10>") 'menu-bar-mode)
+
+;; Use shell-like backspace C-h, rebind help to F1
+(define-key key-translation-map [?\C-h] [?\C-?])
+(global-set-key (kbd "<f1>") 'help-command)
+
+;; Transpose stuff with M-t
+(global-unset-key (kbd "M-t")) ;; which used to be transpose-words
+(global-set-key (kbd "M-t l") 'transpose-lines)
+(global-set-key (kbd "M-t w") 'transpose-words)
+(global-set-key (kbd "M-t s") 'transpose-sexps)
+(global-set-key (kbd "M-t p") 'transpose-params)
+
+;; Change next underscore with a camel case
+(global-set-key (kbd "C-c C--") 'replace-next-underscore-with-camel)
+(global-set-key (kbd "M-s M--") 'snakeify-current-word)
+
+;; Killing text
+(global-set-key (kbd "C-w") 'kill-region-or-backward-word)
+(global-set-key (kbd "C-S-k") 'kill-and-retry-line)
+
+;; Use M-w for copy-line if no active region
+(global-set-key (kbd "M-w") 'save-region-or-current-line)
+(global-set-key (kbd "M-W") '(lambda () (interactive) (save-region-or-current-line 1)))
+
+;; Make shell more convenient, and suspend-frame less
+(global-set-key (kbd "C-z") 'shell)
+(global-set-key (kbd "C-x C-z") 'suspend-frame)
+
+;; Zap to char
+(global-set-key (kbd "M-z") 'zap-to-char)
+(global-set-key (kbd "M-Z") 'zap-to-char-exclusive)
+
+;; iy-go-to-char - like f in Vim
+(global-set-key (kbd "M-m") 'jump-char-forward)
+(global-set-key (kbd "M-M") 'jump-char-backward)
+
+;; Remap old M-m to M-i (better mnemonic for back-to-indentation)
+;; We lose tab-to-tab-stop, which is no big loss in my use cases.
+(global-set-key (kbd "M-i") 'back-to-indentation)
+
+;; Font size
+(define-key global-map (kbd "C-+") 'zoom-frm-in)
+(define-key global-map (kbd "C--") 'zoom-frm-out)
+
+;; Create new frame
+(define-key global-map (kbd "C-x C-n") 'make-frame-command)
+
+;; Jump to a definition in the current file. (This is awesome)
+(global-set-key (kbd "C-x C-i") 'ido-imenu)
+
+;; File finding
+(global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
+(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+(global-set-key (kbd "C-x C-p") 'find-or-create-file-at-point)
+(global-set-key (kbd "C-x M-p") 'find-or-create-file-at-point-other-window)
+(global-set-key (kbd "C-c y") 'bury-buffer)
+(global-set-key (kbd "C-c r") 'revert-buffer)
+(global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Edit file with sudo
+(global-set-key (kbd "M-s e") 'sudo-edit)
+
+;; Window switching
+(windmove-default-keybindings) ;; Shift+direction
+(global-set-key (kbd "C-x -") 'rotate-windows)
+
+;; Indentation help
+(global-set-key (kbd "M-j") (lambda () (interactive) (join-line -1)))
+
+;; Fetch the contents at a URL, display it raw.
+(global-set-key (kbd "C-x h") 'view-url)
+
+;; Help should search more than just commands
+(global-set-key (kbd "<f1> a") 'apropos)
+
+;; Should be able to eval-and-replace anywhere.
+(global-set-key (kbd "C-c e") 'eval-and-replace)
+
+;; Navigation bindings
+(global-set-key (kbd "C-x g") 'goto-line)
+(global-set-key (kbd "<home>") 'beginning-of-buffer)
+(global-set-key (kbd "<end>") 'end-of-buffer)
+(global-set-key (kbd "M-<up>") 'beginning-of-buffer)
+(global-set-key (kbd "M-<down>") 'end-of-buffer)
+(global-set-key (kbd "M-p") 'backward-paragraph)
+(global-set-key (kbd "M-n") 'forward-paragraph)
+(global-set-key (kbd "C-e") 'move-end-of-line-or-next-line)
+(global-set-key (kbd "C-a") 'move-start-of-line-or-prev-line)
+
+;; Move more quickly
+(global-set-key (kbd "C-S-n") (lambda () (interactive) (next-line 5)))
+(global-set-key (kbd "C-S-p") (lambda () (interactive) (previous-line 5)))
+(global-set-key (kbd "C-S-f") (lambda () (interactive) (forward-char 5)))
+(global-set-key (kbd "C-S-b") (lambda () (interactive) (backward-char 5)))
+;; Convenience on ThinkPad Keyboard: Use back/forward as pg up/down
+
+(global-set-key (kbd "<XF86Back>") 'scroll-down)
+(global-set-key (kbd "<XF86Forward>") 'scroll-up)
+(global-set-key (kbd "<XF86WakeUp>") 'beginning-of-buffer)
+
+;; Query replace regex key binding
+(global-set-key (kbd "M-&") 'query-replace-regexp)
+
+;; Yank selection in isearch
+(define-key isearch-mode-map (kbd "C-o") 'isearch-yank-selection)
+
+;; Comment/uncomment block
+(global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-c u") 'uncomment-region)
+
+;; Eval buffer
+(global-set-key (kbd "C-c v") 'eval-buffer)
+
+;; Create scratch buffer
+(global-set-key (kbd "C-c b") 'create-scratch-buffer)
+
+;; Move windows, even in org-mode
+(global-set-key (kbd "<s-right>") 'windmove-right)
+(global-set-key (kbd "<s-left>") 'windmove-left)
+(global-set-key (kbd "<s-up>") 'windmove-up)
+(global-set-key (kbd "<s-down>") 'windmove-down)
+
+;; Mark all
+(global-set-key (kbd "C-c a") 'mark-whole-buffer)
+
+;; Eval lines
+(global-set-key (kbd "C-c C-e") 'eval-current-line)
+(global-set-key (kbd "C-c M-e") 'eval-output-marked-lines)
+
+;; Magit
+(global-set-key (kbd "C-x m") 'magit-status)
+
+;; Clever newlines
+(global-set-key (kbd "<C-return>") 'new-line-below)
+(global-set-key (kbd "<C-S-return>") 'new-line-above)
+(global-set-key (kbd "<M-return>") 'new-line-in-between)
+
+;; Duplicate region
+(global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
+
+;; Line movement
+(global-set-key (kbd "<C-S-down>") 'move-line-down)
+(global-set-key (kbd "<C-S-up>") 'move-line-up)
+
+;; Yank and indent
+(global-set-key (kbd "C-S-y") 'yank-indented)
+
+;; Toggle quotes
+(global-set-key (kbd "C-\"") 'toggle-quotes)
+
+;; Sorting
+(global-set-key (kbd "M-s l") 'sort-lines)
+
+;; Paste URLs as HTML links
+(global-set-key (kbd "C-c C-l") 'linkify-from-kill-ring)
+
+;; Browse the kill ring
+(global-set-key (kbd "C-x C-y") 'browse-kill-ring)
+
+;; Buffer file functions
+(global-set-key (kbd "C-x t") 'touch-buffer-file)
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
+
+;; Dired jump
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+
+;; Easy-mode fullscreen rgrep
+(global-set-key (kbd "M-s s") 'rgrep-fullscreen)
+
+;; Find-name-dired
+(global-set-key (kbd "M-s f") 'find-name-dired)
+
+;; Find file in project
+(global-set-key (kbd "C-x p") 'find-file-in-project)
+
+(global-set-key (kbd "C-x C-o") 'next-multiframe-window)
+
+(provide 'key-bindings)
